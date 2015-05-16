e5eb1e49a95487b5f8b5a309d2fb7d340cb6c899
Xiaosea <maysrp@gmail.com>
2015-05-16 13:43:14 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,15 @@
 #!bin/bash
 # goagent
 
-https://codeload.github.com/goagent/goagent/zip/3.0
-wget -O /home/goagent.zip https://codeload.github.com/goagent/goagent/zip/3.0
-unzip -o /home/goagent -d /home
-rm goagent.zip
-cat /home/goagent-3.0/local/proxy.ini |more
-rm -rf /home/goagent-3.0
+sj=$(date +"%Y%md")
+
+read -p "add your dir" ad
+read -p "your mysql root password " pss
+read -p "your databases " dat
+
+cd $ad
+mysqldump -u root -p --password $pass $dat>$sj.sql
+cd ..
+tar -czf $sj.tar.gz $ad
+mv $sj.tar.gz $ad
+
